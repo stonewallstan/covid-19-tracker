c08341f95e4f86522fc3d4eedf2eff20fc5443a5
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2020-03-30 23:09:58 -0700

    finished the location component, onto data for US

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Header } from 'react-native-elements';
+import Locations from './components/location';
 
 export default function App() {
   return (
@@ -10,19 +11,8 @@ export default function App() {
         centerComponent = {{text: 'Covid-19 Tracker', style: { color: '#FCF7F8' }}}
         containerStyle= {{backgroundColor: '#275DAD'}}
       />
+      <Locations />
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    flex: 1,
-    backgroundColor: '#275DAD',
-    color: '#FCF7F8',
-    fontSize: 'large',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '1rem' 
-  },
-});
--- components/location.tsx
+++ components/location.tsx
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { Constants} from 'expo';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import { MAPS_KEY } from 'react-native-dotenv';
 
-class Location extends Component {
-    constructor(props) {
-       super(props);
-       this.state = {}; 
+class Locations extends Component {
+    state = {
+        errorMessage: null,
+        state: null
+    }; 
+
+    async componentDidMount() {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState(
+                {
+                errorMessage: 'Location Access Denied'
+                }
+            );
+            return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        const latitude = JSON.stringify(location.coords.latitude);
+        const longitude = JSON.stringify(location.coords.longitude);
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json',
+            {
+              params: {
+                latlng: latitude + ', ' + longitude,
+                result_type: 'administrative_area_level_1',
+                key: MAPS_KEY 
+            }
+        }).then(response => {
+            alert(JSON.stringify(response.data));
+            const state = JSON.stringify(response.data.results[0].address_components[0].short_name);
+            this.setState({
+                state
+            });
+        }).catch(error => {
+            alert(error);
+        });
     }
+
     render() {
+        let text = '';
+        if (this.state.errorMessage) {
+            text = this.state.errorMessage;
+        }
+        else if (this.state.state) {
+            text = 'Current State: ' + this.state.state;
+        }
+        console.log(this.state.state);
         return (
             <View>
-                <Text>
-                    Where are you?
-                </Text>
+                <TouchableOpacity>
+                    <Text>
+                       Where are you?
+                    </Text>
+                    <Text>
+                        { text }
+                    </Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
-export default Location;
+export default Locations;
